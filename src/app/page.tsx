49bd5ed21cbb5b1f6d3c79588801e7556b8b8788
Prime-Emmanuel <malongueprime@gmail.com>
2026-06-02 20:53:12 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Process from "@/components/Process";
-import Gallery from "@/components/Gallery";
-import Testimonials from "@/components/Testimonials";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -13,8 +11,6 @@ export default function Home() {
       <Hero />
       <WhyChooseUs />
       <Process />
-      <Gallery />
-      <Testimonials />
       <Footer />
     </main>
   );
